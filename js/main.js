678b262a85d1e5ae5336ab27821aa98687362eae
Clementine Brown <clementine@clmntnbrn.co.uk>
2015-12-02 16:02:11 +0000

    burger nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
+$(document).ready(function(){
+
 $(function(){
- var shrinkHeader = 200;
+  var shrinkHeader = 200;
   $(window).scroll(function() {
     var scroll = getCurrentScroll();
       if ( scroll >= shrinkHeader ) {
-           $('.header-content').addClass('shrink');
-        }
-        else {
-            $('.header-content').removeClass('shrink');
-        }
-  });
+        $('.header-content').addClass('shrink');
+      }
+      else {
+        $('.header-content').removeClass('shrink');
+      }
+});
+
 function getCurrentScroll() {
     return window.pageYOffset || document.documentElement.scrollTop;
     }
+});
+
+  $('.burger').on('click', function(event){
+      event.preventDefault();
+      $('.site-nav ul').slideToggle();
+  });
+  $(window).on('resize', function(){
+      if ($(window).width() > 860) {
+        $('.site-nav ul').show();
+        $('.nav-icon').hide();
+      }
+      else {
+        $('.site-nav ul').hide();
+        $('.nav-icon').show();
+      }
+  });
+
 });
