0f97bc3eb79368d6b3a3651ccbba30d5ecd144a4
Clementine Brown <clementine@clmntnbrn.co.uk>
2015-12-14 14:10:03 +0000

    responsive sub nav

--- js/main.js
+++ js/main.js
@@ -32,6 +32,42 @@ $(document).ready(function(){
       $('.nav-icon').show();
     }
   });
+  //breadcrumbs
+  $('.breadcrumbs-burger').on('click', function(event){
+      event.preventDefault();
+      $('.breadcrumbs ul').slideToggle('easeInOut');
+  });
+  $(window).on('resize', function(){
+    if ($(window).width() > 860) {
+      $('.breadcrumbs ul').show();
+      $('.nav-icon').hide();
+    }
+    else {
+      $('.breadcrumbs ul').hide();
+      $('.nav-icon').show();
+    }
+  });
+  // //scroll up
+  // $(function () {
+  //   $.scrollUp({
+  //     scrollName: 'scrollUp', // Element ID
+  //     topDistance: '300', // Distance from top before showing element (px)
+  //     topSpeed: 300, // Speed back to top (ms)
+  //     animation: 'fade', // Fade, slide, none
+  //     animationInSpeed: 200, // Animation in speed (ms)
+  //     animationOutSpeed: 200, // Animation out speed (ms)
+  //     scrollText: '⌃', // Text for element
+  //   });
+  // });
+
+  // //scroll reveal
+  //   var config = {
+  //         easing: 'ease-in',
+  //         reset:  true,
+  //         delay:  'onload',
+  //         vFactor: 0.10
+  //     }
+  //   window.sr = new scrollReveal();
 
   //filter
   // $('.sort').click(function(event) {
@@ -79,126 +115,126 @@ $(document).ready(function(){
   //       $('.sub-nav').slideDown();
   //     }
   // });
+  //sub nav
+  // //asia
+  // // when I click on the asia list item
+  // $('.primary-nav .asia').on('click', function(event){
+  //   event.preventDefault();
 
-  //asia
-  // when I click on the asia list item
-  $('.primary-nav .asia').on('click', function(event){
-    event.preventDefault();
+  //   // if I click asia again then
+  //   if ( $('.primary-nav .asia').hasClass('display') ) {
 
-    // if I click asia again then
-    if ( $('.primary-nav .asia').hasClass('display') ) {
-
-      // remove the display class
-      $('.primary-nav .asia').removeClass('display');
+  //     // remove the display class
+  //     $('.primary-nav .asia').removeClass('display');
     
-      // slide the mega nav up
-      $('.sub-nav').slideUp();
-    }
-    else {
-      // remove the display class previous nav item
-      $('.primary-nav li').removeClass('display');
+  //     // slide the mega nav up
+  //     $('.sub-nav').slideUp();
+  //   }
+  //   else {
+  //     // remove the display class previous nav item
+  //     $('.primary-nav li').removeClass('display');
 
-      // add an display class to the item
-      $('.primary-nav .asia').addClass('display');
+  //     // add an display class to the item
+  //     $('.primary-nav .asia').addClass('display');
   
-      // hide all the sections
-      $('.drop').hide();
+  //     // hide all the sections
+  //     $('.drop').hide();
   
-      // show the asia section
-      $('.sub-nav .asia').show();
+  //     // show the asia section
+  //     $('.sub-nav .asia').show();
   
-      // the mega nav slides down
-      $('.sub-nav').slideDown();
-    }
-  });
-  //australasia
-  $('.primary-nav .australasia').on('click', function(event){
-    event.preventDefault();
+  //     // the mega nav slides down
+  //     $('.sub-nav').slideDown();
+  //   }
+  // });
+  // //australasia
+  // $('.primary-nav .australasia').on('click', function(event){
+  //   event.preventDefault();
 
-    if ( $('.primary-nav .australasia').hasClass('display') ) {
+  //   if ( $('.primary-nav .australasia').hasClass('display') ) {
 
-      $('.primary-nav .australasia').removeClass('display');
+  //     $('.primary-nav .australasia').removeClass('display');
 
-      $('.sub-nav').slideUp();
-    }
-    else {
-      $('.primary-nav li').removeClass('display');
+  //     $('.sub-nav').slideUp();
+  //   }
+  //   else {
+  //     $('.primary-nav li').removeClass('display');
 
-      $('.primary-nav .australasia').addClass('display');
+  //     $('.primary-nav .australasia').addClass('display');
 
-      $('.drop').hide();
+  //     $('.drop').hide();
 
-      $('.sub-nav .australasia').show();
+  //     $('.sub-nav .australasia').show();
 
-      $('.sub-nav').slideDown();
-    }
-  });
-  //americas
-  $('.primary-nav .americas').on('click', function(event){
-    event.preventDefault();
+  //     $('.sub-nav').slideDown();
+  //   }
+  // });
+  // //americas
+  // $('.primary-nav .americas').on('click', function(event){
+  //   event.preventDefault();
 
-    if ( $('.primary-nav .americas').hasClass('display') ) {
+  //   if ( $('.primary-nav .americas').hasClass('display') ) {
 
-      $('.primary-nav .americas').removeClass('display');
+  //     $('.primary-nav .americas').removeClass('display');
 
-      $('.sub-nav').slideUp();
-    }
-    else {
-      $('.primary-nav li').removeClass('display');
+  //     $('.sub-nav').slideUp();
+  //   }
+  //   else {
+  //     $('.primary-nav li').removeClass('display');
 
-      $('.primary-nav .americas').addClass('display');
+  //     $('.primary-nav .americas').addClass('display');
 
-      $('.drop').hide();
+  //     $('.drop').hide();
 
-      $('.sub-nav .americas').show();
+  //     $('.sub-nav .americas').show();
 
-      $('.sub-nav').slideDown();
-    }
-  });
-  //europe
-  $('.primary-nav .europe').on('click', function(event){
-    event.preventDefault();
+  //     $('.sub-nav').slideDown();
+  //   }
+  // });
+  // //europe
+  // $('.primary-nav .europe').on('click', function(event){
+  //   event.preventDefault();
 
-    if ( $('.primary-nav .europe').hasClass('display') ) {
+  //   if ( $('.primary-nav .europe').hasClass('display') ) {
 
-      $('.primary-nav .europe').removeClass('display');
+  //     $('.primary-nav .europe').removeClass('display');
 
-      $('.sub-nav').slideUp();
-    }
-    else {
-      $('.primary-nav li').removeClass('display');
+  //     $('.sub-nav').slideUp();
+  //   }
+  //   else {
+  //     $('.primary-nav li').removeClass('display');
 
-      $('.primary-nav .europe').addClass('display');
+  //     $('.primary-nav .europe').addClass('display');
 
-      $('.drop').hide();
+  //     $('.drop').hide();
 
-      $('.sub-nav .europe').show();
+  //     $('.sub-nav .europe').show();
 
-      $('.sub-nav').slideDown();
-    }
-  });
-  //scandinavia
-  $('.primary-nav .scandinavia').on('click', function(event){
-    event.preventDefault();
+  //     $('.sub-nav').slideDown();
+  //   }
+  // });
+  // //scandinavia
+  // $('.primary-nav .scandinavia').on('click', function(event){
+  //   event.preventDefault();
 
-    if ( $('.primary-nav .scandinavia').hasClass('display') ) {
+  //   if ( $('.primary-nav .scandinavia').hasClass('display') ) {
 
-      $('.primary-nav .scandinavia').removeClass('display');
+  //     $('.primary-nav .scandinavia').removeClass('display');
 
-      $('.sub-nav').slideUp();
-    }
-    else {
-      $('.primary-nav li').removeClass('display');
+  //     $('.sub-nav').slideUp();
+  //   }
+  //   else {
+  //     $('.primary-nav li').removeClass('display');
 
-      $('.primary-nav .scandinavia').addClass('display');
+  //     $('.primary-nav .scandinavia').addClass('display');
 
-      $('.drop').hide();
+  //     $('.drop').hide();
 
-      $('.sub-nav .scandinavia').show();
+  //     $('.sub-nav .scandinavia').show();
 
-      $('.sub-nav').slideDown();
-    }
-  });
+  //     $('.sub-nav').slideDown();
+  //   }
+  // });
 
   //remove margin
   // $('.container').on('load', function(){
